1d4dc1863d4bf7905666847a723a1a582b2bda39
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-05 14:44:55 +0700

    [CUAN-002][homepage] init itemCard and implement looping v-for on it

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -1,90 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ItemCard from "./items/ItemCard.vue";
+
+const itemList = ref([
+  { img: "items-1.jpg", title: "Mobile UI Kit", category: "Mobile UI Kit" },
+  {
+    img: "items-2.jpg",
+    title: "Online Doctor Consultation",
+    category: "Website UI Kit",
+  },
+  { img: "items-3.jpg", title: "Banking Crypto", category: "Mobile UI Kit" },
+]);
+</script>
+
 <template>
   <div class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-        <div class="overflow-hidden border border-gray-200 rounded-xl">
-          <a href="details.html">
-            <div class="m-4 overflow-hidden rounded-xl">
-              <img
-                alt="Placeholder"
-                class="block w-full h-auto"
-                src="src/assets/img/items-1.jpg"
-              />
-            </div>
-          </a>
-
-          <header class="px-4 mb-4 leading-tight">
-            <h1 class="text-lg">
-              <a
-                class="font-semibold text-black no-underline hover:underline"
-                href="#"
-              >
-                Mobile UI Kit
-              </a>
-            </h1>
-            <span class="block text-sm font-light text-gray-500 no-underline">
-              Mobile UI Kit
-            </span>
-          </header>
-        </div>
-      </div>
-      <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-        <div class="overflow-hidden border border-gray-200 rounded-xl">
-          <a href="details.html">
-            <div class="m-4 overflow-hidden rounded-xl">
-              <img
-                alt="Placeholder"
-                class="block w-full h-auto"
-                src="src/assets/img/items-2.jpg"
-              />
-            </div>
-          </a>
-
-          <header class="px-4 mb-4 leading-tight">
-            <h1 class="text-lg">
-              <a
-                class="font-semibold text-black no-underline hover:underline"
-                href="#"
-              >
-                Online Doctor Consultation
-              </a>
-            </h1>
-            <span class="block text-sm font-light text-gray-500 no-underline">
-              Website UI Kit
-            </span>
-          </header>
-        </div>
-      </div>
-      <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-        <div class="overflow-hidden border border-gray-200 rounded-xl">
-          <a href="details.html">
-            <div class="m-4 overflow-hidden rounded-xl">
-              <img
-                alt="Placeholder"
-                class="block w-full h-auto"
-                src="src/assets/img/items-3.jpg"
-              />
-            </div>
-          </a>
-
-          <header class="px-4 mb-4 leading-tight">
-            <h1 class="text-lg">
-              <a
-                class="font-semibold text-black no-underline hover:underline"
-                href="#"
-              >
-                Banking Crypto
-              </a>
-            </h1>
-            <span class="block text-sm font-light text-gray-500 no-underline">
-              Mobile UI Kit
-            </span>
-          </header>
-        </div>
-      </div>
+      <!-- CARD LIST -->
+      <ItemCard
+        v-for="(item, index) in itemList"
+        :key="index"
+        :img="item.img"
+        :title="item.title"
+        :category="item.category"
+      />
     </div>
   </div>
 </template>
+
 <style></style>

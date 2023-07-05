46aa0524c95ab0ad885c2eead36d240cad35e137
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-05 14:06:24 +0700

    [CUAN-002][Homepage] init categories component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Categories from "../components/homepage/Categories.vue";
 import Hero from "../components/homepage/Hero.vue";
 </script>
 
 <template>
   <main>
     <Hero />
+    <Categories />
   </main>
 </template>

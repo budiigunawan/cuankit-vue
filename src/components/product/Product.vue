ee15811da53a2860489ed1aa7351321298ce5ca0
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-07 11:33:01 +0700

    [CUAN-004][Product] slice main product component

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,5 +1,58 @@
 <script setup></script>
 
 <template>
-  <div>Ini Product Component</div>
+  <div class="container p-2 mx-auto my-10 max-w-7xl">
+    <div class="flex flex-row flex-wrap py-4">
+      <div role="main" class="w-full px-4 pt-1 sm:w-2/3 md:w-2/3">
+        <h1
+          class="mb-2 text-3xl font-bold leading-normal tracking-tight text-gray-900 sm:text-4xl md:text-4xl"
+        >
+          RoboCrypto UI Kit
+        </h1>
+        <p class="text-gray-500">Build your next coin startup</p>
+        <section id="gallery">
+          <img
+            src="src/assets/img/gallery-1.png"
+            alt=""
+            class="w-full mt-6 rounded-2xl"
+          />
+          <div class="grid grid-cols-4 gap-4 mt-4">
+            <div
+              class="overflow-hidden cursor-pointer ring-2 ring-indigo-500 rounded-2xl"
+            >
+              <img src="src/assets/img/gallery-3.png" class="w-full" alt="" />
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+              <img src="src/assets/img/gallery-2.png" class="w-full" alt="" />
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+              <img src="src/assets/img/gallery-4.png" class="w-full" alt="" />
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+              <img src="src/assets/img/gallery-5.png" class="w-full" alt="" />
+            </div>
+          </div>
+        </section>
+        <section class="" id="orders">
+          <h1 class="mt-8 mb-3 text-lg font-semibold">About</h1>
+          <div class="text-gray-500">
+            <p class="pb-4">
+              Sportly App UI Kit will help your Sport, Fitness, and Workout App
+              products or services. Came with modern and sporty style, you can
+              easily edit and customize all elements with components that can
+              speed up your design process.
+            </p>
+            <p class="pb-4">
+              Suitable for : <br />
+              - Sport App <br />
+              - Fitness & GYM App <br />
+              - Workout App <br />
+              - Trainer & Tracker App <br />
+              - And many more <br />
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
 </template>

856b0d6c1b58ed6bbb7789aa25e6cef076cf79e9
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-18 16:52:09 +0700

    [CUAN-006][Success Page] create success components, add success route, and change route destination in some link

--- src/components/pricing/plans/PlanCard.vue
+++ src/components/pricing/plans/PlanCard.vue
@@ -35,7 +35,7 @@ function showPrice(price) {
         </li>
       </ul>
       <RouterLink
-        to="/checkout"
+        to="/success"
         class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-md md:px-10 hover:shadow"
       >
         Checkout Now

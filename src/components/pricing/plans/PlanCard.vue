91fc63ac9efccbd471ea643fd6eeb15e553278fb
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-18 14:48:36 +0700

    [CUAN-005][Pricing] add plans and plan card component

---
+++ src/components/pricing/plans/PlanCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink } from "vue-router";
+const props = defineProps({
+  plan: Object,
+});
+
+function showPrice(price) {
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumSignificantDigits: 2,
+  });
+
+  return formatter.format(price);
+}
+</script>
+
+<template>
+  <div>
+    <div class="p-8 border rounded-3xl">
+      <h1 class="text-5xl font-semibold">
+        {{ showPrice(plan.price) + " " }}
+        <span class="text-xl font-light text-gray-500">/month</span>
+      </h1>
+      <h2 class="text-lg font-semibold mt-7">{{ plan.title }}</h2>
+      <p class="mb-6 text-gray-500">{{ plan.subTitle }}</p>
+      <ul v-if="plan.features.length" class="mb-6 text-gray-700">
+        <li class="mb-3" v-for="(feature, index) in plan.features" :key="index">
+          <img
+            src="src/assets/img/icon-check.png"
+            class="float-left w-6 mr-2"
+            alt=""
+          />
+          {{ feature }}
+        </li>
+      </ul>
+      <RouterLink
+        to="/checkout"
+        class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-md md:px-10 hover:shadow"
+      >
+        Checkout Now
+      </RouterLink>
+    </div>
+  </div>
+</template>

e9099342a6bc8c1833c22c10da3a6a5d7e79ecfa
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-07-20 09:12:31 +0700

    [CUAN-006][Cuankit] add hamburger button on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/flowbite/**/*.js"],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}','./node_modules/flowbite/**/*.js'],
   theme: {
     fontFamily: {
       sans: ['Poppins', 'sans-serif']

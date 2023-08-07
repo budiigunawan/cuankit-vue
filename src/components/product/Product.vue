58be77f5d288570a2b97eed1460e4da263867888
Budi Indra Gunawan <budi.gunawan@xtremax.com>
2023-08-07 20:17:22 +0700

    [CUAN-009][Product Detail] make product main component in product detail dynamic

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,12 +1,42 @@
 <script setup>
+import { ref, onMounted, inject } from "vue";
+import { useRoute } from "vue-router";
+
 import ProductMain from "./ProductMain.vue";
 import ProductAside from "./ProductAside.vue";
+
+const api = inject("$api");
+const route = useRoute();
+
+const productData = ref({});
+
+async function getProductData() {
+  api.get(
+    `products?id=${route.params.id}`,
+    (resp) => {
+      productData.value = resp;
+    },
+    (err) => {
+      console.error(err);
+    }
+  );
+}
+
+onMounted(() => {
+  getProductData();
+});
 </script>
 
 <template>
   <div class="container p-2 mx-auto my-10 max-w-7xl">
     <div class="flex flex-row flex-wrap py-4">
-      <ProductMain />
+      <ProductMain
+        :name="productData?.name"
+        :subtitle="productData?.subtitle"
+        :galleries="productData?.galleries"
+        :thumbnails="productData?.thumbnails"
+        :description="productData?.description"
+      />
       <ProductAside />
     </div>
   </div>

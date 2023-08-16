58b42602e8c2360391eca80f74e824b6cb749b1d
Budi Gunawan <budiindra.g@gmail.com>
2023-08-16 11:56:00 +0700

    hide navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
-import Navbar from "./components/navbar/Navbar.vue";
+// import Navbar from "./components/navbar/Navbar.vue";
 </script>
 
 <template>
-  <Navbar />
+  <!-- <Navbar /> -->
   <main>
     <RouterView />
   </main>
